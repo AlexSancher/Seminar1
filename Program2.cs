75456b7c1c791c6b337aab7f1b46157c7d68a12d
ALS <alexsanch@mail.ru>
2022-06-20 09:49:24 +0300

    Program2

--- Program2.cs
+++ Program2.cs
@@ -1,5 +1,5 @@
 void SnowNumber(){
-    int num = new Random().Next (10, 100);
+    int num = new Random().Next(10, 100);
     int firstNum = num / 10;
     int secondNum = num % 10;
     Console.WriteLine(num + "->");
