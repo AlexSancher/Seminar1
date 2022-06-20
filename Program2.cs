89c684fc19db7089bd335bb71b1f9b8320fa51c3
ALS <alexsanch@mail.ru>
2022-06-20 09:41:58 +0300

    Program2

---
+++ Program2.cs
@@ -0,0 +1,13 @@
+void SnowNumber (){
+    int num = new Random (). Next (10, 100);
+    int firstNum = num / 10;
+    int secondNum = num % 10;
+    Console.WriteLine (num + " -> ");
+    if (firstNum > secondNum){Console.WriteLine(firstNum);
+    }
+    else{
+        Console.WriteLine(secondNum);
+    }
+}
+
+SnowNumber();
