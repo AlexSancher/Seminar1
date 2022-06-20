e83a207b9967ffa1befa80eb7b54e9be1e174dcb
ALS <alexsanch@mail.ru>
2022-06-20 10:27:07 +0300

    Program2

--- Program2.cs
+++ Program2.cs
@@ -1,13 +1,41 @@
-void SnowNumber(){
-    int num = new Random().Next(10, 100);
-    int firstNum = num / 10;
-    int secondNum = num % 10;
-    Console.WriteLine(num + "->");
-    if (firstNum > secondNum){
-        Console.WriteLine(firstNum);
-    }
-    else{
-        Console.WriteLine(secondNum);
-    }
-}
-SnowNumber();
+// void SnowNumber(){
+//     int num = new Random().Next(10, 100);
+//     int firstNum = num / 10;
+//     int secondNum = num % 10;
+//     Console.WriteLine(num + "->");
+//     if (firstNum > secondNum){
+//         Console.WriteLine(firstNum);
+//     }
+//     else{
+//         Console.WriteLine(secondNum);
+//     }
+// }
+// SnowNumber();
+
+// int NumberWithoutSecDig(int Number){
+//     int NewNumber = 0;
+//     NewNumber = Number / 100 * 10 + Number % 10;
+//     return NewNumber;
+// }
+// int Number = new Random().Next(100, 1000);
+// int a  = NumberWithoutSecDig(Number);
+// Console.WriteLine(a + " " + Number);
+
+
+// void MultiNum(int a, int b){
+//     if(b%a ==0){
+//         Console.WriteLine(b + " кратно " + a);
+//     }else{
+//         Console.WriteLine(b + " не кратно " + a + ", остаток " + b%a);
+//     }
+// }
+
+// Console.Write("Введите последовательно два числа:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// MultiNum(num1, num2);
+
+
+дз
+
