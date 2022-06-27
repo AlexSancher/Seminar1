beea8f4297afb136ace3d9886390d9868f0233e2
ALS <alexsanch@mail.ru>
2022-06-28 00:51:57 +0300

    Program4

--- Program4.cs
+++ Program4.cs
@@ -45,15 +45,85 @@
 // Console.WriteLine(countnumbers(numbers));
 
 
-int miltiplayToNum(int num)
+// int miltiplayToNum(int num)
+// {
+//     int miltiplay = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         miltiplay *= i;
+//     }
+//     return miltiplay;
+// }
+
+// int numbers = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(miltiplayToNum (numbers));
+
+// Программа выводит в массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+
+// int [] CreateRandomArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++){
+//         array[i] = new Random().Next(0,2);
+//     }
+//     return array;
+// }
+
+
+
+// // создаем массив из чисел
+// int [] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         Console.Write("Input " + i + "element: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// // выводим значение элементов массива в одну строку через пробел
+
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++){
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+// int [] myRandomArray = CreateRandomArray (8, 0, 1);
+// ShowArray(myRandomArray);
+
+
+
+
+
+
+// ДЗ----------------------------------------------------
+
+
+// #25
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeA = Console.ReadLine();
+int A = Convert.ToInt32(writeA);
+
+int GetNumbersSum(int A)
 {
-    int miltiplay = 1;
-    for (int i = 1; i <= num; i++)
+    int number = 1;
+    int sum = 0;
+    for (int i = 0; i < A; i++)
     {
-        miltiplay *= i;
+        sum = sum + number;
+        number++;
     }
-    return miltiplay;
+    return sum;
 }
 
-int numbers = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(miltiplayToNum (numbers));
+int NumbersSum = GetNumbersSum(A);
+System.Console.WriteLine($"Сумма чисел от 1 до {A} составляет {NumbersSum}");
