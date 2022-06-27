63fd1e7edc09cf4fa455cd821632ac91909d3a5a
ALS <alexsanch@mail.ru>
2022-06-28 00:09:28 +0300

    Program4

--- Program4.cs
+++ Program4.cs
@@ -1,15 +1,29 @@
 // Программа принимает на вход число (А) и выдает сумму чисел от 1 до А
 
-int sumToNum(int num)
-{
-    int sum = 0;
-    for (int i = 1; i <= num; i++)
-    {
-        sum += i;
-    }
-    return sum;
-}
+// int sumToNum(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
 
-Console.WriteLine("Input number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Sum of elements is " + sumToNum(number));
+// Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Sum of elements is " + sumToNum(number));
+
+
+// Программа принимает на вход число и выдает количество цифр в числе
+
+
+int numbers = 5532;
+int count = 0;
+
+while(numbers > 0)
+{
+    numbers = numbers / 10;
+    count++;
+}
+Console.WriteLine(count);
