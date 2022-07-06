3a17035fa2870c5ec6a32dde3bae028e4bf1652d
ALS <alexsanch@mail.ru>
2022-07-06 12:26:18 +0300

    Program8

--- Program8.cs
+++ Program8.cs
@@ -302,4 +302,71 @@
 
 
 
+// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
+
+// int[,,] CreateArray3D(int x, int y, int z)
+// {
+//     int[,,] array3D = new int[x, y, z];
+//     int elem = 10;
+
+//     for (int i = 0; i < array3D.GetLength(0); i++)
+//     {
+//         elem += i;
+//         for (int j = 0; j < array3D.GetLength(1); j++)
+//         {
+//             elem += j;
+//             for (int q = 0; q < array3D.GetLength(2); q++)
+//             {
+//                 elem += q;
+//                 array3D[i, j, q] = elem;
+//             }
+//         }
+//     }
+//     return array3D;
+// }
+
+// void PrintArray3D(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int q = 0; q < array.GetLength(2); q++)
+//             {
+//                 Console.Write($"({i},{j},{q}):- {array[i, j, q]} ");
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// int[,,] triD = CreateArray3D(3, 3, 3);
+// PrintArray3D(triD);
+
+
+// Задача 62: Заполните спирально массив 4 на 4.
+
+
+
+// Console.WriteLine("Введите размер массива:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[n, n];
+// int number = 1;
+// for (int i = 0; i < n; i++)
+// {
+//     for (int j = i; j < n - i; j++) array[i, j] = number++;
+//     for (int k = i + 1; k < n - i; k++) array[k, n - 1 - i] = number++;
+//     for (int j = n - 2 - i; j >= i; j--) array[n - 1 - i, j] = number++;
+//     for (int k = n - 2 - i; k > i; k--) array[k, i] = number++;
+// }
+// for (int i = 0; i < n; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
